34b3b2cd2d0389da3acb0a336a46ee2324ce78d3
Scott Burch <scott@bulldoginfo.com>
2015-11-12 09:44:08 -0800

    removing

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,17 @@
 var RewirePlugin = require("rewire-webpack");
-var path = require('path');
-var glob = require('glob');
-var fs = require('fs');
 var webpack = require('webpack');
-var CommonsChunkPlugin = require("webpack/lib/optimize/CommonsChunkPlugin");
-var BeepPlugin = require('webpack-beep-plugin');
-
-var commonsChunkPlugin = new CommonsChunkPlugin({
-    name: "vendor",
-    filename: 'vendor.js',
-     minChunks: Infinity,
-});
-
-var sourceMap = new webpack.SourceMapDevToolPlugin({
-    test:/\.js$/,
-    exclude: 'vendor.js',
-});
 
 module.exports = {
-    entry: {
-        vendor: ['react', 'react-dom', 'react-bootstrap', 'reactive-store', 'lodash', 'jquery', 'properties'],
-        component: glob.sync(__dirname+'/entry.js')
-    },
+    entry: './entry.js',
     output: {
-        filename: '[name].tests.js'
+        filename: 'entry.out.js'
     },
     module: {
         loaders: [
-            { test: /.*messagebundles.*/, loader: 'raw'},
             { test: /\.js$/, loader: 'babel-loader' }
         ]
     },
-    node: {
-        fs: "empty"
-    },
     plugins: [
-        new RewirePlugin(),
-        sourceMap,
-        commonsChunkPlugin,
-        new BeepPlugin()
+        new RewirePlugin()
     ]
 };
