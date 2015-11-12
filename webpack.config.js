e0abd83b61caf5613a1372e6b124d5fc9d1b576d
Scott Burch <scott@bulldoginfo.com>
2015-11-12 09:52:04 -0800

    removing

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 var RewirePlugin = require("rewire-webpack");
-var webpack = require('webpack');
 
 module.exports = {
     entry: './entry.js',
@@ -11,7 +10,5 @@ module.exports = {
             { test: /\.js$/, loader: 'babel-loader' }
         ]
     },
-    plugins: [
-        new RewirePlugin()
-    ]
+    plugins: [new RewirePlugin()]
 };
